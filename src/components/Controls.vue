a17aa3f55991beb98721d363bfaaa27c639d0876
Quinn Pollock <contact@quinnpollock.net>
2021-07-04 14:56:26 -0700

    Revert "move around definition"
    This reverts commit 3c661ae0628729b8d6ca2537505ef15c418ee73a.

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -35,7 +35,12 @@
 
 <script setup>
 	import { ref, watch } from 'vue'
+	
+	const currentYear = new Date().getFullYear()
+	const selectedYear = ref(currentYear)
 
+	const urlParams = new URLSearchParams(window.location.search)
+	const username = ref(urlParams.get("u"))
 	const emit = defineEmit(["changeUsername", "changeYear"])
 
 	const props = defineProps({
@@ -44,16 +49,6 @@
 			default: 2011
 		}
 	})
-	
-	const currentYear = new Date().getFullYear()
-	const urlParams = new URLSearchParams(window.location.search)
-
-	const selectedYear = ref(currentYear)
-	const username = ref(urlParams.get("u"))
-
-	if (username != null) {
-		emit("changeYear", username)
-	}
 
 	const range = (start, end) => {
 		if (selectedYear.value < end) {
