5c8430d5f3fd3b94b4187bebd4767aa97f41f0a3
Quinn Pollock <contact@quinnpollock.net>
2021-07-04 16:42:00 -0700

    Update Graph.vue

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -78,7 +78,7 @@
 
 	const getLink = (year, day) => {
 		const date = getDate(year, day);
-		return `https://letterboxd.com/${props.username}/films/diary/for/${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${(date.getDate()).toString().padStart(2, '0')}/`
+		return props.username != "" ? `https://letterboxd.com/${props.username}/films/diary/for/${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${(date.getDate()).toString().padStart(2, '0')}/` : ""
 
 
 	}
